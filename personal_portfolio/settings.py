a78f03087d5b36e786914a16bc06771b9098a079
shabiasaeed <Shabia-ab@hotmail.com>
2021-11-28 18:33:32 -0500

    changed local setting again

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8vv6*#r6t*in9+^ej#-uu1pio3)buzso#8bmq_a2nfrh*pr4__
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.shabiasaeed.com']
+ALLOWED_HOSTS = ['www.shabiasaeed.com', '*']
 
 
 # Application definition
