a45afb1b5d9e1fdc63ccda6b6415763809f55c10
shabiasaeed <Shabia-ab@hotmail.com>
2021-11-28 19:34:14 -0500

    deploying changes

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,10 +123,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-#try:
-    #from .local_settings import *
-#except ImportError:
-    #print("Looks like no local file. You must be on production")
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
